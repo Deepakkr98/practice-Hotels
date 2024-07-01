b09a9299ec00f9b7f65546f66d6c0d85860c5b5d
Deepak <dkraj2798@gmail.com>
2024-07-01 15:56:52 +0530

    shift mongodb in online server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Define the MongoDB connection URL
-const mongoURL = "mongodb://127.0.0.1:27017/hotels"; // Replace 'hotels' with your database name
+// const mongoURL = "mongodb://127.0.0.1:27017/hotels"; // Replace 'hotels' with your database name
+const mongoURL = process.env.MONGODB_URL;
 
 // set up MongoDB connection
 mongoose.connect(mongoURL);
